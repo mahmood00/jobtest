a6bfe9371e026f8567e425c8f0d2b273ebf20239
Mahmood Damery <ma.damery0@gmail.com>
2016-04-02 21:02:49 +0300

    Update log.js

--- log.js
+++ log.js
@@ -1,19 +1,4 @@
-function loadjscssfile(filename, filetype){
-    if (filetype=="js"){ //if filename is a external JavaScript file
-        var fileref=document.createElement('script')
-        fileref.setAttribute("type","text/javascript")
-        fileref.setAttribute("src", filename)
-    }
-    else if (filetype=="css"){ //if filename is an external CSS file
-        var fileref=document.createElement("link")
-        fileref.setAttribute("rel", "stylesheet")
-        fileref.setAttribute("type", "text/css")
-        fileref.setAttribute("href", filename)
-    }
-}
- 
-loadjscssfile("https://code.jquery.com/jquery-1.12.0.min.js", "js") //dynamically load and add this .js file
-loadjscssfile("http://momentjs.com/downloads/moment.min.js", "js") //dynamically load and add this .js file
+
 $(document).ready(function(){
 	$( ".moment" ).each(function() {
 		$(this).text( moment($(this).text()).fromNow() );
